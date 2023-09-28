9f22673e801669156322d4f45d957e198ae0d864
Pham Thi Huong Quynh <pthquynh2001@gmail.com>
2023-09-28 11:56:15 +0700

    final pagination and query params

--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -5,37 +5,41 @@ import Pagination from '../Pagination';
 import queryString from 'query-string';
 import { useEffect, useState } from 'react';
 import { NEW_PRODUCTS_URL } from '../api/apiUrls';
-import { useNavigate } from 'react-router-dom';
-// import { useNavigate, useLocation } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
 const Products = () => {
-  // const location = useLocation();
+  const location = useLocation();
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  // const [paramString, setParamString] = useState('limit=12&page=1');
+  const [paramString, setParamString] = useState(
+    location.search ? location.search : '?limit=12&page=1'
+  );
   const [pagination, setPagination] = useState({
     page: 1,
     limit: 12,
     totalItems: 31,
   });
-  const [filters, setFilters] = useState({
-    page: 1,
-    limit: 12,
-  });
+  const [filters, setFilters] = useState(
+    location.search
+      ? queryString.parse(location.search)
+      : {
+          page: 1,
+          limit: 12,
+        }
+  );
 
-  // 1.1
   useEffect(() => {
     const fetchData = async () => {
       try {
         // const pramString = queryString.stringify(filters);
         setLoading(true);
-        const response = await fetch(
-          `${NEW_PRODUCTS_URL}?${queryString.stringify(filters)}`
-        );
+        const response = await fetch(`${NEW_PRODUCTS_URL}${paramString}`);
         const data = await response.json();
+        navigate(`${paramString}`);
         setLoading(false);
         setProducts(data);
       } catch (err) {
@@ -43,48 +47,9 @@ const Products = () => {
       }
     };
     fetchData();
-  }, [filters]);
+  }, [paramString, navigate]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       // const pramString = queryString.stringify(filters);
-  //       setLoading(true);
-  //       const response = await fetch(`${NEW_PRODUCTS_URL}?${paramString}`);
-  //       const data = await response.json();
-  //       setLoading(false);
-  //       setProducts(data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [paramString]);
-
-  // th1- nhan location tu btn
-  // function handlePageChange(newPage) {
-  //   setPagination({ ...pagination, page: newPage });
-  //   setFilters({ ...filters, page: newPage });
-  //   if (!loading) {
-  //     window.scrollTo(0, 500);
-  //     // productsRef.current.scrollTop = 10;
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   setParamString(`?${queryString.stringify(filters)}`);
-  //   navigate(`?${queryString.stringify(filters)}`);
-  //   console.log(paramString);
-  // }, [filters, navigate, paramString]);
-
-  // // th2 - nhan location tu url
-  // const params = location.search;
-
-  // useEffect(() => {
-  //   setParamString(params);
-  // }, [params]);
-
-  //1.2
+  //th1- nhan location tu btn
   function handlePageChange(newPage) {
     setPagination({ ...pagination, page: newPage });
     setFilters({ ...filters, page: newPage });
@@ -93,10 +58,49 @@ const Products = () => {
       // productsRef.current.scrollTop = 10;
     }
   }
-  //1.3
+
   useEffect(() => {
-    navigate(`?${queryString.stringify(filters)}`);
-  }, [filters, navigate]);
+    setParamString(`?${queryString.stringify(filters)}`);
+    // console.log(paramString);
+  }, [filters]);
+
+  // th2 - nhan location tu url
+  // const params = location.search;
+  // useEffect(() => {
+  //   setParamString(params);
+  // }, [params]);
+
+  // //1.1
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       // const pramString = queryString.stringify(filters);
+  //       setLoading(true);
+  //       const response = await fetch(
+  //         `${NEW_PRODUCTS_URL}?${queryString.stringify(filters)}`
+  //       );
+  //       const data = await response.json();
+  //       setLoading(false);
+  //       setProducts(data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [filters]);
+  //1.2
+  // function handlePageChange(newPage) {
+  //   setPagination({ ...pagination, page: newPage });
+  //   setFilters({ ...filters, page: newPage });
+  //   if (!loading) {
+  //     window.scrollTo(0, 500);
+  //     // productsRef.current.scrollTop = 10;
+  //   }
+  // }
+  // //1.3
+  // useEffect(() => {
+  //   navigate(`?${queryString.stringify(filters)}`);
+  // }, [filters, navigate]);
 
   return (
     <div className={cx('wrapper')}>
